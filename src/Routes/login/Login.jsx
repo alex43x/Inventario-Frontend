3db84f8c6180158f75a52312bf44378fb539c81c
Alex <alexgllamosas@gmail.com>
2025-03-02 12:46:29 -0300

    .

--- src/Routes/login/Login.jsx
+++ src/Routes/login/Login.jsx
@@ -15,6 +15,7 @@ function Login() {
     e.preventDefault();
 
     try {
+      console.log("RUTA",process.env.REACT_APP_API_URL)
       const response = await fetch(`${process.env.REACT_APP_API_URL}/login`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
