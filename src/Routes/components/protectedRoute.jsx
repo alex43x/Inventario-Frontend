aeb6d006056a41c3256051c0bb91a0133c60c9f5
Alex <alexgllamosas@gmail.com>
2025-03-04 09:43:44 -0300

    Auth

--- src/Routes/components/protectedRoute.jsx
+++ src/Routes/components/protectedRoute.jsx
@@ -3,7 +3,11 @@ import { Navigate, Outlet } from "react-router-dom";
 import { useAuth } from "./authContext";
 
 const ProtectedRoute = () => {
-  const { isAuthenticated } = useAuth();
+  const { isAuthenticated, isLoading } = useAuth();
+
+  if (isLoading) {
+    return <div>Cargando...</div>; // Puedes cambiar esto por un spinner o algo más elegante
+  }
 
   if (!isAuthenticated) {
     console.log("Acceso denegado: Redirigiendo a login...");
